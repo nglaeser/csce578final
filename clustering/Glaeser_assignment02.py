5be96d9a67264a9e39faafeb1b6a2519bda31dd4
nglaeser <nglaeser@users.noreply.github.com>
2019-04-20 15:53:13 -0400

    clean code and file paths

--- clustering/Glaeser_assignment02.py
+++ clustering/Glaeser_assignment02.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[202]:
-
-
+import os
 import json
 from pprint import pprint
 from datetime import datetime
@@ -12,35 +10,53 @@ import math
 from collections import defaultdict
 import numpy as np
 
-
-# In[203]:
-
-
 punctlist = ['.', '!', '?', ',', ';', ':', '"', "'", '(', ')', '--', '-', '...', '/', '\'', '@',
             '&', '>', '<', '=', '’', '^', '…', '_']
 # split on punctuation and get rid of it
 
-stopwords = set(line.strip() for line in open('../stopwords.txt'))
-stopwords.update(['www', 'http', 'https', 'com'])
+########### USEFUL FUNCTIONS ########################
+def dot(arr1, arr2):
+    if len(arr1) != len(arr2):
+        print("vector dimensions don't match")
+        return -1
+    
+    dot = 0
+    for i in range(len(arr1)):
+        dot += arr1[i]*arr2[i]
+    return dot
+        
+def norm(arr):
+    norm = 0
+    for i in range(len(arr)):
+        norm += math.pow(arr[i], 2)
+    return math.sqrt(float(norm))
+#####################################################
 
+try:
+    project_dir = os.environ['PROJECT_PATH']
+except:
+    print("Please make sure you have set your PROJECT_PATH variable as described in the readme.")
+    exit(0)
 
-# In[204]:
+try:
+    stopword_file = os.path.join(project_dir, 'utilities/stopwords.txt')
+    stopwords = set(line.strip() for line in open(stopword_file))
+    stopwords.update(['www', 'http', 'https', 'com'])
+except:
+    print("Stopword file " + str(stopword_file) + "not found")
+    exit(0)
 
-
-with open('transcript-15528094.json') as f:
-    data = json.load(f)
-
-#pprint(data[0])
-
-
-# In[205]:
-
+transcript_filename = input("Enter the name of your transcript file: ")
+try:
+    transcript_filepath = os.path.join(project_dir, transcript_filename)
+    with open(transcript_filepath) as f:
+        data = json.load(f)
+except:
+    print("Could not open the file " + str(transcript_filename) + " in " + os.path.split(transcript_filepath)[:-1][0])
+    exit(0)
 
 id_to_name = defaultdict(str)
 doc_wordcounts = defaultdict(lambda: defaultdict(int)) # each document has a dict of word counts
-
-
-# In[206]:
 
 
 for item in data:
@@ -70,16 +86,6 @@ for item in data:
         if user_id not in id_to_name:
             id_to_name[user_id] = item['name']
         
-        #print("%s: %s" % (item['name'], item['text']))
-        
-#pprint(doc_wordcounts.keys())
-#pprint(doc_wordcounts['21235813'])
-#pprint(id_to_name)
-
-
-# In[208]:
-
-
 # now turn these raw counts into frequencies (normalize by doc length)
 for doc in doc_wordcounts:
     word_counts = doc_wordcounts[doc]
@@ -88,10 +94,6 @@ for doc in doc_wordcounts:
         length += word_counts[word]
     for word in word_counts:
         word_counts[word] /= length
-
-
-# In[209]:
-
 
 # now get idf for each term
 dfs_t = defaultdict(int)
@@ -107,22 +109,13 @@ for term in termlist:
             dfs_t[term] += 1
 
 num_docs = len(doc_wordcounts)
-#print(num_docs)
 for term in termlist:
     idfs_t[term] = math.log(num_docs/float(dfs_t[term]))
-
-
-# In[210]:
-
 
 tf_idfs = defaultdict(lambda: defaultdict(float))
 for doc in doc_wordcounts:
     for term in doc_wordcounts[doc]:
         tf_idfs[doc][term] = doc_wordcounts[doc][term] * idfs_t[term]
-
-
-# In[221]:
-
 
 # now, in each document, sort terms by tf-idf
 # put top 10 into dict of vectors
@@ -130,8 +123,7 @@ vectors = defaultdict(list)
 
 top_tfidf_words = set()
 for doc in tf_idfs:
-    #print('\n**** user %s **** ' % id_to_name[doc])
-    
+
     # top ten words per doc by tf-idf
     for key_value_pair in sorted(tf_idfs[doc].items(),
            key=lambda k_v: k_v[1],
@@ -141,52 +133,20 @@ for doc in tf_idfs:
         tf_idf = key_value_pair[1]
         
         top_tfidf_words.add(word)
-        
-        #print("%s:\t%f" % (word, tf_idf))
     
 top_tfidf_words = list(top_tfidf_words)
-#print(len(top_tfidf_words))
 
 for doc in tf_idfs:
     vec = []
     for word in top_tfidf_words:
         vec.append(tf_idfs[doc][word])
     vectors[doc] = vec
-#print(len(vectors.keys()))
-
-
-# In[213]:
-
-
-def dot(arr1, arr2):
-    if len(arr1) != len(arr2):
-        print("vector dimensions don't match")
-        return -1
-    
-    dot = 0
-    for i in range(len(arr1)):
-        dot += arr1[i]*arr2[i]
-    return dot
-        
-def norm(arr):
-    norm = 0
-    for i in range(len(arr)):
-        norm += math.pow(arr[i], 2)
-    return math.sqrt(float(norm))
-
-
-# In[214]:
 
 
 # make cluster matrix
 matrix = np.zeros((num_docs, num_docs), dtype=float)
 i = 0
 j = 0
-
-# pprint(vectors.keys())
-# for doc in vectors:
-#     print(doc)
-# # check that they're in the same order
 
 for doc1 in vectors:
     j=0
@@ -210,59 +170,22 @@ for doc1 in vectors:
         j += 1
     i += 1
 
+fakename = defaultdict(str)
 
-# In[184]:
+fakename_file = os.path.join(project_dir, 'utilities/fakenames.txt')
+try:
+    f = open(fakename_file, 'r')
+except:
+    print("Could not find fakenames.txt in the utilities directory of your project path.")
+    exit(0)
 
-
-#pprint(matrix)
-
-
-# In[215]:
-
-
-fake_name = defaultdict(str)
-fake_name = {'10820250': 'Jack',
-             '13207027': 'John',
-             '14137183': 'Peter',
-             '18559389': 'Anna',
-             '18559680': 'Ethan',
-             '19458971': 'Alex',
-             '20963086': 'Kylie',
-             '21235741': 'Jim',
-             '21235813': 'Nathan',
-             '21241679': 'Aaron',
-             '21349729': 'Sarah',
-             '21755924': 'Bob',
-             '22207515': 'Claire',
-             '22276941': 'Sally',
-             '23354087': 'Mike',
-             '24927292': 'Tim',
-             '26498711': 'Nina',
-             '26508082': 'Jacob',
-             '26514517': 'Lisa',
-             '26601370': 'Mitch',
-             '270093': 'Alan',
-             '270280': 'tom',
-             '27380802': 'Jill',
-             '27546073': 'Walter',
-             '28068527': 'Simon',
-             '28222063': 'Annie',
-             '28880161': 'Chris',
-             '29652195': 'Nancy',
-             '29666163': 'Michael',
-             '29747722': 'Jason',
-             '29750591': 'Trey',
-             '29775461': 'Nicole',
-             '29775466': 'Liza',
-             '29818898': 'Thomas',
-             '29846726': 'Isaac',
-             '45033570': 'Patrick',
-             '46185459': 'Zo',
-             '51427894': 'Natalie'}
-
-
-# In[218]:
-
+try:
+    for line in f:
+        tokens = line.split()
+        fakename[tokens[0]] = tokens[1]
+except:
+    print("The file utilities/fakenames.txt is improperly formatted.")
+    exit(0)
 
 doc_indices = list(vectors.keys())
 similarities = defaultdict(float)
@@ -272,26 +195,14 @@ for i in range(len(matrix)):
     for j in range(i, len(matrix)):
         if i == j:
             continue
-        doc_pair = (fake_name[doc_indices[i]], fake_name[doc_indices[j]])
+        doc_pair = (fakename[doc_indices[i]], fakename[doc_indices[j]])
         similarities[doc_pair] = matrix[i][j]
-#         if matrix[i][j] > 0.5:
-#             print("%s and %s have a cosine of %s" % (doc_indices[i], doc_indices[j], matrix[i][j]))
-
-
-# In[219]:
-
 
 # print top ten similar docs
+print("***** Top ten similar documents by tfidf *****")
 for key_value_pair in sorted(similarities.items(),
            key=lambda k_v: k_v[1],
            reverse=True)[:10]:
     doc_pair = key_value_pair[0]
     cosine = key_value_pair[1]
     print("%s:\t\t%s" % (doc_pair, cosine))
-
-
-# In[ ]:
-
-
-
-
